b79068d7068aab4069ae5e1ee6ef7c1792e340b5
Max Wu <chengmaxwu@gmail.com>
2022-04-19 01:53:20 -0700

    Refetch user data after sign in

--- web/hooks/useSignIn.ts
+++ web/hooks/useSignIn.ts
@@ -1,18 +1,23 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useCallback } from "react";
 import { auth } from "../lib/firebase";
+import { useUserData } from "./useUserData";
 
 const googleOauthProvider = new GoogleAuthProvider();
 
 export function useSignIn() {
+  const { refetch } = useUserData();
+
   const afterSignIn = useCallback(async () => {
     const token = await auth.currentUser?.getIdToken();
     return fetch(`/api/auth/upsertUserData`, {
       headers: {
         authorization: `Bearer ${token}`,
       },
+    }).then(() => {
+      refetch({ requestPolicy: "network-only" });
     });
-  }, []);
+  }, [refetch]);
 
   const signInWithGoogle = useCallback(async () => {
     signInWithPopup(auth, googleOauthProvider).then(afterSignIn);
