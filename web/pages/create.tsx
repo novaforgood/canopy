625b625e2e78d95ae959432875226e212a94a065
Max Wu <chengmaxwu@gmail.com>
2022-04-18 21:44:03 -0700

    Add automatic slug generation

--- web/pages/create.tsx
+++ web/pages/create.tsx
@@ -1,30 +1,44 @@
 import React, { useEffect, useState } from "react";
+import { customAlphabet } from "nanoid";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Button } from "../components/atomic/Button";
 import { Input } from "../components/atomic/Input";
-import { useCreateSpaceMutation } from "../generated/graphql";
+import { useCreateOwnerProfileInNewSpaceMutation } from "../generated/graphql";
 import { useUserData } from "../hooks/useUserData";
 import { auth } from "../lib/firebase";
 
+const nanoid = customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 8);
+
+function slugify(name: string) {
+  return name
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
+
+function slugifyAndAppendRandomString(name: string) {
+  return `${slugify(name)}-${nanoid()}`;
+}
+
 export default function CreatePage() {
-  const [res, createSpace] = useCreateSpaceMutation();
+  const [res, createOwnerProfile] = useCreateOwnerProfileInNewSpaceMutation();
 
   const { userData } = useUserData();
   const [name, setName] = useState("");
-  const [slug, setSlug] = useState("");
-
-  console.log(name, slug);
 
   return (
     <div>
       <div>Create a space!</div>
       <Input value={name} onValueChange={setName} placeholder="Name" />
-      <Input value={slug} onValueChange={setSlug} placeholder="Slug" />
       <Button
         onClick={() => {
-          createSpace({
-            space_name: name,
-            slug: slug,
+          createOwnerProfile({
+            space: {
+              name,
+              owner_id: userData?.id,
+              slug: slugifyAndAppendRandomString(name),
+            },
             user_id: userData?.id ?? "",
           });
         }}
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { useIsLoggedIn } from "../hooks/useIsLoggedIn";
 
 const Home: NextPage = () => {
+  const isLoggedIn = useIsLoggedIn();
   return (
     <div className="bg-blue-50">
       <Link href="/login">Login</Link>
