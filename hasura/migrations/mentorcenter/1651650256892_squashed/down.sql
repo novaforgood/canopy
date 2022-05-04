7fd762924a966ebda71ebf3f0a3f775e8885a996
Max Wu <chengmaxwu@gmail.com>
2022-05-04 00:46:21 -0700

    Rename all tables to singular

---
+++ hasura/migrations/mentorcenter/1651650256892_squashed/down.sql
@@ -0,0 +1,14 @@
+
+alter table "public"."user" rename to "users";
+
+alter table "public"."space" rename to "spaces";
+
+alter table "public"."space_invite_link" rename to "space_invite_links";
+
+alter table "public"."space_invite_link_type" rename to "space_invite_link_types";
+
+alter table "public"."profile_to_profile_role" rename to "profiles_profile_roles";
+
+alter table "public"."profile" rename to "profiles";
+
+alter table "public"."profile_role" rename to "profile_roles";
