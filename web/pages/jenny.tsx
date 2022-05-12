3da049ac6afdb690a65562e99ab87ebdf999b1a0
jennywnuo <jennywnuo@gmail.com>
2022-05-12 16:00:39 -0700

    better card components with person type

--- web/pages/jenny.tsx
+++ web/pages/jenny.tsx
@@ -12,30 +12,39 @@ const ppl = new Array(64).fill(0).map((i) => {
   };
 });
 
-function Card(person: any) {
-  console.log(person);
+type Person = {
+  name: string;
+  lname: string;
+  job: string;
+  company: string;
+  pic: string;
+  description: string;
+};
+
+function Card(props: { person: Person }) {
+  console.log(props);
   const TOPICS = "Topics";
   return (
     <div className="w-72 border-gray-400 mb-10 border-x border-y rounded-md">
       <div className="h-72 w-full border-none pb-4">
         <img
           className="w-full rounded-t-md"
-          src={person.person.pic}
-          alt={person.person.name}
+          src={props.person.pic}
+          alt={props.person.name}
         />
       </div>
       <div className="px-8">
         <div className="pb-2 pt-4 text-2xl font-semibold">
-          {person.person.name} {person.person.lname}
+          {props.person.name} {props.person.lname}
         </div>
         <div className="mb-4 text-md pb-6">
-          {person.person.job} at {person.person.company}
+          {props.person.job} at {props.person.company}
         </div>
         <div className="pb-2">
           <i>{TOPICS}</i>
         </div>
         <div className="mb-4 text-gray-500 text-ellipsis overflow-hidden">
-          {person.person.description}
+          {props.person.description}
         </div>
       </div>
     </div>
