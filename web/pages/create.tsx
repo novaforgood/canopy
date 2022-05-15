9a6425a6c8f0d6d4e27302a0a6f3bcc831f636e8
Max Wu <chengmaxwu@gmail.com>
2022-05-15 04:57:15 -0700

    Add stage nav

--- web/pages/create.tsx
+++ web/pages/create.tsx
@@ -5,6 +5,8 @@ import { Input } from "../components/atomic/Input";
 import { useCreateOwnerProfileInNewSpaceMutation } from "../generated/graphql";
 import { useUserData } from "../hooks/useUserData";
 import { useRouter } from "next/router";
+import { useQueryParam } from "../hooks/useQueryParam";
+import classNames from "classnames";
 
 const nanoid = customAlphabet("0123456789abcdefghijklmnopqrstuvwxyz", 6);
 
@@ -28,6 +30,88 @@ function makeReadableError(message: string) {
   }
 }
 
+enum CreateStage {
+  EnterName = "EnterName",
+  EnterHomepagePhoto = "EnterHomepagePhoto",
+  EnterProfileSchema = "EnterProfileSchema",
+  EnterSettings = "EnterSettings",
+}
+
+const MAP_STAGE_TO_LABEL: Record<CreateStage, string> = {
+  [CreateStage.EnterName]: "Name",
+  [CreateStage.EnterHomepagePhoto]: "Homepage Photo",
+  [CreateStage.EnterProfileSchema]: "Mentor Profiles",
+  [CreateStage.EnterSettings]: "Directory Settings",
+};
+
+const ALL_CREATE_STAGES = Object.values(CreateStage).map((val) => {
+  return {
+    value: val,
+    label: MAP_STAGE_TO_LABEL[val],
+  };
+});
+
+interface StageNavigatorProps<TStageEnum> {
+  currentStage: TStageEnum;
+  stages: {
+    value: TStageEnum;
+    label: string;
+  }[];
+  onStageClick?: (stage: TStageEnum) => void;
+}
+function StageNavigator<TStageEnum extends string>(
+  props: StageNavigatorProps<TStageEnum>
+) {
+  const { currentStage, stages, onStageClick = () => {} } = props;
+
+  const [hoveredStage, setHoveredStage] = useState<TStageEnum | null>(null);
+
+  return (
+    <div className="flex flex-col gap-4 w-full text-gray-700">
+      {stages.map(({ value, label }, idx) => {
+        const isCurrent = value === currentStage;
+        const isHovered = hoveredStage === value;
+
+        const cardStyles = classNames({
+          "bg-white flex w-full items-center border-2 transition cursor-pointer":
+            true,
+          "border-gray-300": !isCurrent && !isHovered,
+          "border-gray-900": !isCurrent && isHovered,
+          "border-black": isCurrent,
+        });
+
+        const labelStyles = classNames({
+          "px-6 py-3 border-l-2 transition": true,
+          "border-gray-300": !isCurrent && !isHovered,
+          "border-gray-900": !isCurrent && isHovered,
+          "border-black": isCurrent,
+        });
+
+        return (
+          <div
+            className={cardStyles}
+            key={value}
+            onClick={() => {
+              onStageClick(value);
+            }}
+            onMouseEnter={() => {
+              setHoveredStage(value);
+            }}
+            onMouseLeave={() => {
+              setHoveredStage(null);
+            }}
+          >
+            <div className="w-14 flex justify-center items-center">
+              {idx + 1}
+            </div>
+            <div className={labelStyles}>{label}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
 export default function CreatePage() {
   const [_, createOwnerProfile] = useCreateOwnerProfileInNewSpaceMutation();
 
@@ -38,47 +122,66 @@ export default function CreatePage() {
   const { userData } = useUserData();
   const [name, setName] = useState("");
 
-  return (
-    <div className="p-4">
-      <div className="text-xl">Create a space!</div>
-      <div className="h-4"></div>
-      <Input
-        value={name}
-        onValueChange={(value) => {
-          setName(value);
-          setError(null);
-        }}
-        placeholder="Name of your new space"
-      />
-      {error && <div className="text-red-500">{error}</div>}
-      <div className="h-4"></div>
-      <Button
-        // disabled={!name}
-        onClick={() => {
-          if (!userData?.id) {
-            return;
-          }
+  const currentStage =
+    useQueryParam("stage", "string") || CreateStage.EnterName;
 
-          const generatedSlug = slugifyAndAppendRandomString(name);
-          createOwnerProfile({
-            space: {
-              name,
-              owner_id: userData.id,
-              slug: generatedSlug,
-            },
-            user_id: userData.id,
-          }).then((result) => {
-            if (result.error) {
-              const msg = makeReadableError(result.error.message);
-              setError(msg);
-            } else {
-              router.push(`/space/${generatedSlug}`);
+  return (
+    <div className="flex h-screen">
+      <div className="bg-gray-50 p-12 pt-40 h-screen">
+        <StageNavigator
+          currentStage={currentStage}
+          stages={ALL_CREATE_STAGES}
+          onStageClick={(newStage) => {
+            router.push({
+              pathname: "/create",
+              query: {
+                stage: newStage,
+              },
+            });
+          }}
+        />
+      </div>
+      <div>
+        <div className="text-xl">Create a space!</div>
+        <div className="h-4"></div>
+        <Input
+          value={name}
+          onValueChange={(value) => {
+            setName(value);
+            setError(null);
+          }}
+          placeholder="Name of your new space"
+        />
+        {error && <div className="text-red-500">{error}</div>}
+        <div className="h-4"></div>
+        <Button
+          // disabled={!name}
+          onClick={() => {
+            if (!userData?.id) {
+              return;
             }
-          });
-        }}
-      >
-        Create
-      </Button>
+
+            const generatedSlug = slugifyAndAppendRandomString(name);
+            createOwnerProfile({
+              space: {
+                name,
+                owner_id: userData.id,
+                slug: generatedSlug,
+              },
+              user_id: userData.id,
+            }).then((result) => {
+              if (result.error) {
+                const msg = makeReadableError(result.error.message);
+                setError(msg);
+              } else {
+                router.push(`/space/${generatedSlug}`);
+              }
+            });
+          }}
+        >
+          Create
+        </Button>
+      </div>
     </div>
   );
 }
