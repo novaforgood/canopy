25184792c8d62fe6cbe7a2aeebce6df1da23265c
jennywnuo <jennywnuo@gmail.com>
2022-05-26 18:07:30 -0700

    all users query and displays users on space

--- web/pages/jenny.tsx
+++ web/pages/jenny.tsx
@@ -1,9 +1,7 @@
 import React, { ReactNode, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
-import { usePersonNameQuery } from "../generated/graphql";
 import { useCurrentProfile } from "../hooks/useCurrentProfile";
-import { useUserData } from "../hooks/useUserData";
-import { useAllProfilesOfUserQuery } from "../generated/graphql";
+import { Card } from "../components/Card";
 
 // const id = currentProfile?.id;
 
@@ -19,50 +17,6 @@ const ppl = new Array(4).fill(0).map((i) => {
     description: faker.lorem.paragraph(),
   };
 });
-
-type Person = {
-  name: string;
-  lname: string;
-  job: string;
-  company: string;
-  pic: string;
-  description: string;
-};
-
-function Card(props: { person: Person }) {
-  const { userData } = useUserData();
-  const [{ data: profileData }] = useAllProfilesOfUserQuery({
-    variables: { user_id: userData?.id ?? "" },
-  });
-  console.log(profileData?.profile);
-  console.log("whore");
-  const TOPICS = "Topics";
-  return (
-    <div className="w-72 border-gray-400 mb-10 border-x border-y rounded-md">
-      <div className="h-72 w-full border-none pb-4">
-        <img
-          className="w-full rounded-t-md"
-          src={props.person.pic}
-          alt={props.person.name}
-        />
-      </div>
-      <div className="px-8">
-        <div className="pb-2 pt-4 text-2xl font-semibold">
-          {props.person.name} {props.person.lname}
-        </div>
-        <div className="mb-4 text-md pb-6">
-          {props.person.job} at {props.person.company}
-        </div>
-        <div className="pb-2">
-          <i>{TOPICS}</i>
-        </div>
-        <div className="mb-4 text-gray-500 text-ellipsis overflow-hidden">
-          {props.person.description}
-        </div>
-      </div>
-    </div>
-  );
-}
 
 function SearchBar() {
   const INTERESTS = [
