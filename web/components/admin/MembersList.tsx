13dc44f817fdd8641bc8fab6b27efe209095ef6a
Max Wu <chengmaxwu@gmail.com>
2022-05-30 12:02:37 -0700

    Refactor members list components into files

--- web/components/admin/MembersList.tsx
+++ web/components/admin/MembersList.tsx
@@ -1,84 +1,13 @@
 import { Fragment } from "react";
 
-import { format } from "date-fns";
-import toast from "react-hot-toast";
-
 import {
   Profile_Role_Enum,
-  useProfileByIdQuery,
   useProfilesBySpaceIdQuery,
-  useUpdateProfileRoleMutation,
 } from "../../generated/graphql";
-import { BxsCrown } from "../../generated/icons/solid";
 import { useCurrentSpace } from "../../hooks/useCurrentSpace";
-import { Select } from "../atomic";
 
-const MAP_ROLE_TO_TITLE: Record<Profile_Role_Enum, string> = {
-  [Profile_Role_Enum.Admin]: "Admin",
-  [Profile_Role_Enum.Member]: "Member",
-  [Profile_Role_Enum.MemberWhoCanList]: "Listed Member",
-  [Profile_Role_Enum.Banned]: "Banned",
-};
+import { MemberRow } from "./MemberRow";
 
-const ROLE_SELECT_OPTIONS = Object.values(Profile_Role_Enum).map((role) => {
-  return {
-    label: MAP_ROLE_TO_TITLE[role],
-    value: role,
-  };
-});
-interface MemberRowProps {
-  profileId: string;
-}
-function MemberRow(props: MemberRowProps) {
-  const { profileId } = props;
-  const [{ data }] = useProfileByIdQuery({
-    requestPolicy: "cache-first",
-    variables: { profile_id: profileId },
-  });
-
-  const { currentSpace } = useCurrentSpace();
-
-  const [_, updateProfileRole] = useUpdateProfileRoleMutation();
-  const profile = data?.profile_by_pk;
-
-  if (!profile) {
-    return null;
-  }
-
-  const role = profile.profile_roles[0];
-
-  const { email, first_name, last_name } = profile.user;
-
-  return (
-    <Fragment>
-      <div className="flex items-center">
-        {first_name} {last_name}{" "}
-        {currentSpace?.owner_id === profile.user.id && (
-          <BxsCrown className="h-4 w-4 ml-2" />
-        )}
-      </div>
-      <div>{email}</div>
-      <Select
-        className="w-48"
-        options={ROLE_SELECT_OPTIONS}
-        value={role.profile_role}
-        onSelect={async (newRole) => {
-          if (newRole) {
-            toast.promise(
-              updateProfileRole({ row_id: role.id, profile_role: newRole }),
-              {
-                loading: "Loading",
-                success: `Updated ${first_name}'s role to ${MAP_ROLE_TO_TITLE[newRole]}`,
-                error: "Error when fetching",
-              }
-            );
-          }
-        }}
-      />
-      <div>{format(new Date(profile.created_at), "MMM dd yyyy, h:mm a")}</div>
-    </Fragment>
-  );
-}
 export function MembersList() {
   const { currentSpace } = useCurrentSpace();
 
