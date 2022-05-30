ecda620a3d46e167afc284b1f5c59653134305c0
Max Wu <chengmaxwu@gmail.com>
2022-05-30 11:52:16 -0700

    Add ability to change member roles

--- web/components/admin/MembersList.tsx
+++ web/components/admin/MembersList.tsx
@@ -1,13 +1,30 @@
 import { Fragment } from "react";
 
 import { format } from "date-fns";
+import toast from "react-hot-toast";
 
 import {
+  Profile_Role_Enum,
   useProfileByIdQuery,
   useProfilesBySpaceIdQuery,
+  useUpdateProfileRoleMutation,
 } from "../../generated/graphql";
 import { useCurrentSpace } from "../../hooks/useCurrentSpace";
+import { Select } from "../atomic";
 
+const MAP_ROLE_TO_TITLE: Record<Profile_Role_Enum, string> = {
+  [Profile_Role_Enum.Admin]: "Admin",
+  [Profile_Role_Enum.Member]: "Member",
+  [Profile_Role_Enum.MemberWhoCanList]: "Listed Member",
+  [Profile_Role_Enum.Banned]: "Banned",
+};
+
+const ROLE_SELECT_OPTIONS = Object.values(Profile_Role_Enum).map((role) => {
+  return {
+    label: MAP_ROLE_TO_TITLE[role],
+    value: role,
+  };
+});
 interface MemberRowProps {
   profileId: string;
 }
@@ -18,18 +35,40 @@ function MemberRow(props: MemberRowProps) {
     variables: { profile_id: profileId },
   });
 
+  const [_, updateProfileRole] = useUpdateProfileRoleMutation();
   const profile = data?.profile_by_pk;
 
   if (!profile) {
     return null;
   }
 
+  const role = profile.profile_roles[0];
+
+  const { email, first_name, last_name } = profile.user;
+
   return (
     <Fragment>
-      <div>{profile.user.email}</div>
       <div>
-        {profile.profile_roles.map((role) => role.profile_role).join(", ")}
+        {first_name} {last_name}
       </div>
+      <div>{email}</div>
+      <Select
+        className="w-48"
+        options={ROLE_SELECT_OPTIONS}
+        value={role.profile_role}
+        onSelect={async (newRole) => {
+          if (newRole) {
+            toast.promise(
+              updateProfileRole({ row_id: role.id, profile_role: newRole }),
+              {
+                loading: "Loading",
+                success: `Updated ${first_name}'s role to ${MAP_ROLE_TO_TITLE[newRole]}`,
+                error: "Error when fetching",
+              }
+            );
+          }
+        }}
+      />
       <div>{format(new Date(profile.created_at), "MMM dd yyyy, h:mm a")}</div>
     </Fragment>
   );
@@ -47,7 +86,8 @@ export function MembersList() {
 
   return (
     <div className="">
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-4 gap-2 items-center">
+        <strong>Name</strong>
         <strong>Email</strong>
         <strong>Role</strong>
         <strong>Created At</strong>
