bb78b67c99925e67fc2f15d7396ea56f5c41df7e
Max Wu <chengmaxwu@gmail.com>
2022-06-02 18:31:13 -0700

    Extract EditProfileImageModal to own component

--- web/components/EditProfileListing.tsx
+++ web/components/EditProfileListing.tsx
@@ -1,112 +1,19 @@
-import { useEffect, useRef, useState } from "react";
-
 import { useDisclosure } from "@mantine/hooks";
-import AvatarEditor from "react-avatar-editor";
-import toast from "react-hot-toast";
 
-import {
-  Profile_Role_Enum,
-  useInsertProfileImageMutation,
-  useProfileImageQuery,
-} from "../generated/graphql";
+import { Profile_Role_Enum, useProfileImageQuery } from "../generated/graphql";
 import { BxsPencil } from "../generated/icons/solid";
 import { useCurrentProfile } from "../hooks/useCurrentProfile";
 import { useCurrentSpace } from "../hooks/useCurrentSpace";
 import { useUserData } from "../hooks/useUserData";
-import { uploadImage } from "../lib/image";
 
 import { Button, Text } from "./atomic";
+import { EditProfileImageModal } from "./edit-profile/EditProfileImageModal";
 import { EditResponse } from "./edit-profile/EditResponse";
 import { ProfileSocialsDisplay } from "./edit-socials-info/ProfileSocialsDisplay";
 import { ProfileSocialsModal } from "./edit-socials-info/ProfileSocialsModal";
 import { EditButton } from "./EditButton";
-import { ImageUploader } from "./ImageUploader";
-import { ActionModal } from "./modals/ActionModal";
 import { ProfileImage } from "./ProfileImage";
 import PublishedToggleSwitch from "./PublishedToggleSwitch";
-
-interface EditProfileImageModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-}
-
-function EditProfileImageModal(props: EditProfileImageModalProps) {
-  const { isOpen, onClose } = props;
-
-  const { currentProfile } = useCurrentProfile();
-
-  const [{ data: profileImageData }] = useProfileImageQuery({
-    variables: { profile_id: currentProfile?.id ?? "" },
-  });
-  const [_, insertProfileImage] = useInsertProfileImageMutation();
-  const profileImageUrl =
-    profileImageData?.profile_listing_image[0]?.image.url ?? null;
-
-  const editor = useRef<AvatarEditor | null>(null);
-
-  const [image, setImage] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (profileImageUrl) {
-      setImage(profileImageUrl);
-    }
-  }, [profileImageUrl]);
-
-  return (
-    <ActionModal
-      isOpen={isOpen}
-      actionText={"Save"}
-      onClose={onClose}
-      onAction={async () => {
-        if (!currentProfile) {
-          toast.error("Current profile not defined");
-          return;
-        }
-        const imageData =
-          editor.current?.getImageScaledToCanvas().toDataURL() ?? null;
-
-        // If image exists, upload it
-        if (imageData) {
-          const res = await uploadImage(imageData).catch((err) => {
-            toast.error(err.message);
-            return null;
-          });
-          if (!res) return;
-
-          const imageId = res.data.image.id;
-          await insertProfileImage({
-            image_id: imageId,
-            profile_id: currentProfile.id,
-          });
-          onClose();
-        } else {
-          toast.error("No image selected");
-        }
-      }}
-      secondaryActionText={"Cancel"}
-      onSecondaryAction={onClose}
-    >
-      <div className="flex flex-col items-center px-8">
-        <div className="h-4"></div>
-        <Text variant="heading4">Change Profile Image</Text>
-        <div className="h-8"></div>
-
-        <ImageUploader
-          showZoom
-          showRoundedCrop
-          imageSrc={image}
-          onImageSrcChange={setImage}
-          width={300}
-          height={300}
-          getRef={(ref) => {
-            editor.current = ref;
-          }}
-        />
-        <div className="h-8"></div>
-      </div>
-    </ActionModal>
-  );
-}
 
 function EditProfileImage() {
   const { currentProfile } = useCurrentProfile();
---
+++ web/components/edit-profile/EditProfileImageModal.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useRef, useState } from "react";
+
+import AvatarEditor from "react-avatar-editor";
+import toast from "react-hot-toast";
+
+import {
+  useInsertProfileImageMutation,
+  useProfileImageQuery,
+} from "../../generated/graphql";
+import { useCurrentProfile } from "../../hooks/useCurrentProfile";
+import { uploadImage } from "../../lib/image";
+import { Text } from "../atomic";
+import { ImageUploader } from "../ImageUploader";
+import { ActionModal } from "../modals/ActionModal";
+
+export interface EditProfileImageModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function EditProfileImageModal(props: EditProfileImageModalProps) {
+  const { isOpen, onClose } = props;
+
+  const { currentProfile } = useCurrentProfile();
+
+  const [{ data: profileImageData }] = useProfileImageQuery({
+    variables: { profile_id: currentProfile?.id ?? "" },
+  });
+  const [_, insertProfileImage] = useInsertProfileImageMutation();
+  const profileImageUrl =
+    profileImageData?.profile_listing_image[0]?.image.url ?? null;
+
+  const editor = useRef<AvatarEditor | null>(null);
+
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (profileImageUrl) {
+      setImage(profileImageUrl);
+    }
+  }, [profileImageUrl]);
+
+  return (
+    <ActionModal
+      isOpen={isOpen}
+      actionText={"Save"}
+      onClose={onClose}
+      onAction={async () => {
+        if (!currentProfile) {
+          toast.error("Current profile not defined");
+          return;
+        }
+        const imageData =
+          editor.current?.getImageScaledToCanvas().toDataURL() ?? null;
+
+        // If image exists, upload it
+        if (imageData) {
+          const res = await uploadImage(imageData).catch((err) => {
+            toast.error(err.message);
+            return null;
+          });
+          if (!res) return;
+
+          const imageId = res.data.image.id;
+          await insertProfileImage({
+            image_id: imageId,
+            profile_id: currentProfile.id,
+          });
+          onClose();
+        } else {
+          toast.error("No image selected");
+        }
+      }}
+      secondaryActionText={"Cancel"}
+      onSecondaryAction={onClose}
+    >
+      <div className="flex flex-col items-center px-8">
+        <div className="h-4"></div>
+        <Text variant="heading4">Change Profile Image</Text>
+        <div className="h-8"></div>
+
+        <ImageUploader
+          showZoom
+          showRoundedCrop
+          imageSrc={image}
+          onImageSrcChange={setImage}
+          width={300}
+          height={300}
+          getRef={(ref) => {
+            editor.current = ref;
+          }}
+        />
+        <div className="h-8"></div>
+      </div>
+    </ActionModal>
+  );
+}
