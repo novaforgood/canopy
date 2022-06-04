25ceee513eca3147d2e44163d539f653087e47f0
Max Wu <chengmaxwu@gmail.com>
2022-06-04 03:13:11 -0700

    Add deleted column to space_tag table

---
+++ hasura/migrations/mentorcenter/1654337339987_alter_table_public_space_tag_add_column_deleted/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."space_tag" add column "deleted" boolean
+ not null default 'false';
--- web/components/admin/EditProfileFormat.tsx
+++ web/components/admin/EditProfileFormat.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
+import { useUpsertSpaceProfileSchemaMutation } from "../../generated/graphql";
 import { useCurrentSpace } from "../../hooks/useCurrentSpace";
+import { Button } from "../atomic";
 
 import { EditProfileSchema, EditProfileSchemaData } from "./EditProfileSchema";
 
@@ -25,8 +27,22 @@ export function EditProfileFormat() {
     });
   }, [currentSpace]);
 
+  const [_, upsertSpaceProfileSchema] = useUpsertSpaceProfileSchemaMutation();
+  const saveChanges = useCallback(async () => {
+    await upsertSpaceProfileSchema({
+      space_listing_questions: data.listingQuestions,
+      space_tag_categories: data.tagCategories.map((category) => ({
+        ...category,
+        space_tags: undefined,
+      })),
+    });
+  }, [data.listingQuestions, data.tagCategories, upsertSpaceProfileSchema]);
+
   return (
     <div className="w-full">
+      <Button rounded onClick={saveChanges}>
+        Save changes
+      </Button>
       <EditProfileSchema data={data} onChange={setData} />
     </div>
   );
