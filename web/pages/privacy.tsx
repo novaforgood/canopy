018d2bf12208fced91aa1fc4a1c14eff7ea4aef1
Max Wu <chengmaxwu@gmail.com>
2022-07-20 01:36:37 -0700

    Improve privacy and terms pages

--- web/pages/privacy.tsx
+++ web/pages/privacy.tsx
@@ -1,10 +1,18 @@
-const PrivacyPage = () => {
+import { Footer } from "../components/Footer";
+import { CustomPage } from "../types";
+
+const PrivacyPage: CustomPage = () => {
   return (
-    <iframe
-      className="h-screen w-full"
-      src="https://docs.google.com/document/d/e/2PACX-1vR7b0lQUzxSxYfy5_vNKKb3Uy4irYI0Uwf5c5s-kxfp6oqZspC-Em3nO0mJj0rvYqwLcg9MotSZ0j5G/pub?embedded=true"
-    ></iframe>
+    <div className="flex flex-col h-screen">
+      <iframe
+        className="h-screen w-full overflow-y-auto"
+        src="https://docs.google.com/document/d/e/2PACX-1vR7b0lQUzxSxYfy5_vNKKb3Uy4irYI0Uwf5c5s-kxfp6oqZspC-Em3nO0mJj0rvYqwLcg9MotSZ0j5G/pub?embedded=true"
+      ></iframe>
+      <Footer />
+    </div>
   );
 };
+
+PrivacyPage.showFooter = false;
 
 export default PrivacyPage;
