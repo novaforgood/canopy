1149abf3c67e2ac2aa0d9ccbd8d18cce1d567782
Max Wu <chengmaxwu@gmail.com>
2022-08-18 09:50:53 -0700

    Squash migrations

--- hasura/migrations/mentorcenter/1659349064368_alter_table_public_space_tag_add_unique_label_space_tag_category_id/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."space_tag" add constraint "space_tag_label_space_tag_category_id_key" unique ("label", "space_tag_category_id");
--- hasura/migrations/mentorcenter/1660839836648_alter_table_public_space_listing_question_add_unique_space_id_id/up.sql
+++
@@ -1,2 +0,0 @@
-alter table "public"."space_listing_question" drop constraint "space_listing_question_listing_order_space_id_id_key";
-alter table "public"."space_listing_question" add constraint "space_listing_question_space_id_id_key" unique ("space_id", "id");
