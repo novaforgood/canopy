f61a51d308036c1596fe080079f736f1613a70fe
Max Wu <chengmaxwu@gmail.com>
2022-08-18 10:16:43 -0700

    Re-squash

--- hasura/migrations/mentorcenter/1660839873845_alter_table_public_space_listing_question_drop_constraint_space_listing_question_space_id_id_key/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."space_listing_question" add constraint "space_listing_question_space_id_id_key" unique ("space_id", "id");
--- hasura/migrations/mentorcenter/1660839873845_alter_table_public_space_listing_question_drop_constraint_space_listing_question_space_id_id_key/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."space_listing_question" drop constraint "space_listing_question_space_id_id_key";
