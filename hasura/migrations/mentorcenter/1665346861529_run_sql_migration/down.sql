321c43aa6bf6e5f6ca4520a37e4978602e82640b
Max Wu <chengmaxwu@gmail.com>
2022-10-09 13:38:27 -0700

    Return error message properly from trigger

---
+++ hasura/migrations/mentorcenter/1665346861529_run_sql_migration/down.sql
@@ -0,0 +1,19 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION public.run_before_insert_event_profile_view()
+--  RETURNS trigger
+--  LANGUAGE plpgsql
+-- AS $function$
+--     BEGIN
+--         IF EXISTS (
+--             SELECT FROM event_profile_view
+--             WHERE viewed_profile_id = NEW.viewed_profile_id
+--                 AND viewer_profile_id = NEW.viewer_profile_id
+--                 AND (now() - interval '15 minutes') < created_at
+--         ) THEN
+--             RAISE EXCEPTION USING ERRCODE = '22000', MESSAGE = 'Cannot insert duplicate event within 15 minutes';
+--         END IF;
+--
+--         RETURN NEW;
+--     END;
+-- $function$;
