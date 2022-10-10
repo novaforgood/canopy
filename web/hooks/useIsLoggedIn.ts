272ab734ef6417443ebf35d05551689bffd3c82a
Max Wu <chengmaxwu@gmail.com>
2022-10-10 10:12:40 -0700

    Bugfix

--- web/hooks/useIsLoggedIn.ts
+++ web/hooks/useIsLoggedIn.ts
@@ -3,6 +3,7 @@ import { useAtom } from "jotai";
 import { sessionAtom } from "../lib/jotai";
 
 export function useIsLoggedIn() {
-  const session = useAtom(sessionAtom);
+  const [session] = useAtom(sessionAtom);
+  console.log(session);
   return session !== null;
 }
