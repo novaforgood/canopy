ca185ef3e2a78323f9865c312d9ec8c138673db8
Max Wu <chengmaxwu@gmail.com>
2022-11-10 12:46:54 -0800

    Add icon gen

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -5,6 +5,8 @@ import { Text } from "../components/atomic/Text";
 import { useAllProfilesOfUserQuery } from "../generated/graphql";
 import { useUserData } from "../hooks/useUserData";
 import type { RootStackParams } from "../types/navigation";
+import { View, Image, StyleSheet, ScrollView } from "react-native";
+import { BxCog } from "../generated/icons/regular";
 
 function HomeScreen({ navigation }: StackScreenProps<RootStackParams, "Home">) {
   const spaces = [{ spaceSlug: "test-space-1", spaceName: "Test Space 1" }];
@@ -15,24 +17,37 @@ function HomeScreen({ navigation }: StackScreenProps<RootStackParams, "Home">) {
   });
 
   return (
-    <Box>
+    <Box p={4}>
       <Box>
-        <Text variant="heading3">
+        <Text variant="heading4">
           Welcome to Canopy Mobile, {userData?.first_name}!
         </Text>
-        {profileData?.profile.map((profile) => {
-          return (
-            <Button
-              onPress={() => {
-                navigation.navigate("Directory", {
-                  spaceSlug: profile.space.slug,
-                });
-              }}
-            >
-              {profile.space.name}
-            </Button>
-          );
-        })}
+        <ScrollView>
+          {profileData?.profile.map((profile) => {
+            return (
+              <Box key={profile.id}>
+                <Image
+                  style={{ width: 50, height: 50 }}
+                  source={{
+                    uri: profile.space.space_cover_image?.image.url,
+                  }}
+                />
+                <Box height={10} width={10}>
+                  <BxCog />
+                </Box>
+                <Button
+                  onPress={() => {
+                    navigation.navigate("Directory", {
+                      spaceSlug: profile.space.slug,
+                    });
+                  }}
+                >
+                  {profile.space.name}
+                </Button>
+              </Box>
+            );
+          })}
+        </ScrollView>
       </Box>
     </Box>
   );
