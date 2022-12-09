4a91f7fe5ef16b4f90b4feababf0e21d70bea5c0
Max Wu <chengmaxwu@gmail.com>
2022-12-09 03:12:45 -0800

    Remove misplaced files

--- hasura/migrations/mentorcenter/down.sql
+++
@@ -1,4 +0,0 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- INSERT INTO "user" (id, first_name, last_name, email, "type") VALUES
--- ('bot-BAZe9rWh4x4TYBCpMU8', 'Canopy', 'Bot', 'team+canopybot@joincanopy.org', 'Bot');
