fd0fbf5d6215e2b5ae12520e20c7a22bdd0e6b5b
Max Wu <chengmaxwu@gmail.com>
2022-12-09 03:10:44 -0800

    Remove extraneous code in squashed migration

--- hasura/migrations/mentorcenter/1670582436436_squashed/up.sql
+++ hasura/migrations/mentorcenter/1670582436436_squashed/up.sql
@@ -40,22 +40,6 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 alter table "public"."chat_room" add column "attributes" jsonb
  not null default jsonb_build_object();
 
-CREATE VIEW chat_intro_data AS (
-    WITH chat_intro_rooms AS (
-        SELECT chat_room.id as chat_room_id, attributes ->> 'chatIntroId' as chat_intro_id, COUNT(*) as num_messages FROM chat_room
-        JOIN chat_message ON chat_message.chat_room_id = chat_room.id
-        WHERE attributes ->> 'chatIntroId' IS NOT NULL
-        GROUP BY chat_room.id
-    ), rooms_with_replies AS (
-        SELECT * FROM chat_intro_rooms
-        WHERE num_messages > 1
-    )
-    SELECT chat_intro_id, COUNT(*) as num_rooms_with_replies FROM rooms_with_replies
-    GROUP BY chat_intro_id
-);
-
-DROP VIEW chat_intro_data;
-
 CREATE OR REPLACE VIEW "public"."chat_intro_data" AS 
  WITH chat_intro_rooms AS (
          SELECT chat_room.id AS chat_room_id,
