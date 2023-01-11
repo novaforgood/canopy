5247295b21533dfe58641465f8e0c738fc97da36
Max Wu <chengmaxwu@gmail.com>
2023-01-10 20:31:50 -0800

    mobile: Further configure universal links

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -30,10 +30,6 @@ function filterEnvVariables(env) {
     }, {});
 }
 
-function removeProtocolFromUrl(url) {
-  return url.split("://")[1];
-}
-
 export default {
   expo: {
     name: config.name,
@@ -63,7 +59,8 @@ export default {
       bundleIdentifier: config.appId,
       buildNumber: runtimeVersion.toString(),
       associatedDomains: [
-        `applinks:${removeProtocolFromUrl(process.env["HOST_URL"])}`,
+        `applinks:canopy-git-dev-novaforgood.vercel.app`,
+        `applinks:*.joincanopy.org`,
       ],
     },
     android: {
