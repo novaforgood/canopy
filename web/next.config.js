786e0e61160036e216bcbbf6db9029585d5267f8
Max Wu <chengmaxwu@gmail.com>
2023-02-04 03:15:48 -0800

    web: Make go links redirect to normal page

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  redirects() {
+    return [
+      {
+        source: "/go/:path*",
+        destination: "/:path*",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
