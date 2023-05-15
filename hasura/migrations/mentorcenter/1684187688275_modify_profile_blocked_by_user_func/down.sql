4eb4d3b64eeb45fdfcf320b665f86f59a3d51f86
Max Wu <chengmaxwu@gmail.com>
2023-05-15 14:55:25 -0700

    fix: blocked_by_user logic

---
+++ hasura/migrations/mentorcenter/1684187688275_modify_profile_blocked_by_user_func/down.sql
@@ -0,0 +1,16 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION public.profile_blocked_by_user(profile_row profile, hasura_session json)
+--  RETURNS boolean
+--  LANGUAGE sql
+--  STABLE
+-- AS $function$
+-- SELECT EXISTS (
+--     SELECT 1
+--     FROM block
+--     JOIN profile ON block.blocker_profile_id = profile.id
+--     WHERE block.blocker_profile_id = profile.id
+--         AND block.blocked_profile_id = profile.id
+--         AND profile.user_id = hasura_session ->> 'x-hasura-user-id'
+-- );
+-- $function$;
