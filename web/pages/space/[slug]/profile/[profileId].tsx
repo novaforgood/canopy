f068aed41d45921b8199e2534f75d91e12aae4a8
Max Wu <chengmaxwu@gmail.com>
2023-08-22 00:58:47 -0700

    Remove console logs

--- web/pages/space/[slug]/profile/[profileId].tsx
+++ web/pages/space/[slug]/profile/[profileId].tsx
@@ -245,12 +245,10 @@ const ProfilePage: CustomPage = () => {
   const isMyProfile = profileId === currentProfile?.id;
 
   if (!currentSpace && !fetchingCurrentSpace) {
-    console.log("no space");
     return <PageNotFound />;
   }
 
   if (!profileData?.profile_by_pk?.profile_listing && !fetchingProfileData) {
-    console.log("no profile listing");
     return <PageNotFound />;
   }
 
