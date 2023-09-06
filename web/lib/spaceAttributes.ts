6e49bc254471bab8d099512009d1e662fd46a280
Max Wu <chengmaxwu@gmail.com>
2023-09-06 13:31:42 -0700

    Add ability to enable chat intros by default

--- web/lib/spaceAttributes.ts
+++ web/lib/spaceAttributes.ts
@@ -2,9 +2,11 @@ export type SpaceAttributes = {
   public: boolean;
   allowOnlyPublicMembersToViewProfiles: boolean;
   domainWhitelist: string | null;
+  optUsersInToMatchesByDefault: boolean;
 };
 export const DEFAULT_SPACE_ATTRIBUTES: SpaceAttributes = {
   public: false,
   allowOnlyPublicMembersToViewProfiles: false,
   domainWhitelist: null,
+  optUsersInToMatchesByDefault: false,
 };
